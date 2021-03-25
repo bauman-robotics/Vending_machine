1acea382fbb4d69541d2407d5b4386d2a212c6a7
bauman-robotics <sm7-telega@yandex.ru>
2021-03-25 13:24:30 +0300

    tab

--- web-client/client.py
+++ web-client/client.py
@@ -5,8 +5,8 @@ ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 0.1)    #Open named port
 
 while True:
     for line in ser.read():
-        print( "ans_from_drv", line )
-		requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})
+        print( "ans from drv = ", line )
+        requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})
     response = requests.get('http://178.140.18.140:3141/cell')
     while response.text == "":
         response = requests.get('http://178.140.18.140:3141/cell')
@@ -19,7 +19,7 @@ while True:
             #a = str(cell) + '\n'
             a = str(cell)
             ser.write(str.encode(a))
-            #requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})
+            requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})
     except:
         print("not number")
         requests.post('http://178.140.18.140:3141/cell', data={"status":"not number"})

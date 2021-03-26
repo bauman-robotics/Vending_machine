9a18eb30f1f47f6cbd3e1846e41e2229796a8ba5
bauman-robotics <sm7-telega@yandex.ru>
2021-03-26 10:44:54 +0300

    ans from driver

--- web-client/client.py
+++ web-client/client.py
@@ -4,9 +4,9 @@ import serial
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 0.1)    #Open named port
 
 while True:
-    for line in ser.read():
+    for line in ser.read():   # ans from driver Serial Port
         print( "ans from drv = ", line )
-        requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})
+        requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})  # ans - successful issue
     response = requests.get('http://178.140.18.140:3141/cell')
     while response.text == "":
         response = requests.get('http://178.140.18.140:3141/cell')
@@ -19,7 +19,7 @@ while True:
             #a = str(cell) + '\n'
             a = str(cell)
             ser.write(str.encode(a))
-            requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})
+            #requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})  //for test   ans - successful issue
     except:
         print("not number")
         requests.post('http://178.140.18.140:3141/cell', data={"status":"not number"})

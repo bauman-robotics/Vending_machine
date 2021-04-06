c0efda9508285d7441b03a3828b062ed69a8b2db
bauman-robotics <sm7-telega@yandex.ru>
2021-04-06 14:28:46 +0300

    change of IP address

--- web-client/client.py
+++ web-client/client.py
@@ -6,10 +6,10 @@ ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 0.1)    #Open named port
 while True:
     for line in ser.read():   # ans from driver Serial Port
         print( "ans from drv = ", line )
-        requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})  # ans - successful issue
-    response = requests.get('http://178.140.18.140:3141/cell')
+        requests.post('http://188.32.179.215:3141/cell', data={"status":"", "num":a, "clientID":1})  # ans - successful issue
+    response = requests.get('http://188.32.179.215:3141/cell')
     while response.text == "":
-        response = requests.get('http://178.140.18.140:3141/cell')
+        response = requests.get('http://188.32.179.215:3141/cell')
         time.sleep(0.3)
     try:
         text = response.text
@@ -19,10 +19,10 @@ while True:
             #a = str(cell) + '\n'
             a = str(cell)
             ser.write(str.encode(a))
-            #requests.post('http://178.140.18.140:3141/cell', data={"status":"", "num":a, "clientID":1})  //for test   ans - successful issue
+            #requests.post('http://188.32.179.215:3141/cell', data={"status":"", "num":a, "clientID":1})  //for test   ans - successful issue
     except:
         print("not number")
-        requests.post('http://178.140.18.140:3141/cell', data={"status":"not number"})
+        requests.post('http://188.32.179.215:3141/cell', data={"status":"not number"})
 
 ser.close()       
 

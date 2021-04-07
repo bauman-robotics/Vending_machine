051ac73b8cbd798593304bb35e0aa0b29afa4272
bauman-robotics <sm7-telega@yandex.ru>
2021-04-07 16:05:22 +0300

    led control trow the get-req from server to client

--- web-client/flask_server/app/views.py
+++ web-client/flask_server/app/views.py
@@ -1,5 +1,22 @@
 from app import app
-@app.route('/')
-@app.route('/index')
-def index():
-return "Hello, World!"
+from flask import request
+import serial, time
+
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 0.1)    #Open named port
+
+i = 16  #led number
+a = str(i)
+
+@app.route('/cell', methods=['GET'])
+def cell():
+    try: 
+        i = request.args.get('n')
+        a = str(i)
+        ser.write(str.encode(a))
+        print("OK")
+    except:
+        print("NO")
+    return "Hello, World!"
+   
+
+

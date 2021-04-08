3987acc9b9ce97a51e90c4fd81df7e4ef25c3075
bauman-robotics <sm7-telega@yandex.ru>
2021-04-09 00:10:55 +0300

    Add ans from client to server. Add start/stop Demons

--- web-client/flask_server/app/views.py
+++ web-client/flask_server/app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import request
+from flask import request,Response,jsonify
 import serial, time
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 0.1)    #Open named port
@@ -9,14 +9,23 @@ a = str(i)
 
 @app.route('/cell', methods=['GET'])
 def cell():
+    response = Response() 
     try: 
+        #a = request.form['n']
         i = request.args.get('n')
         a = str(i)
         ser.write(str.encode(a))
         print("OK")
     except:
         print("NO")
-    return "Hello, World!"
+    time.sleep(5)
+    response.headers["Content-Type"]="text/plain"
+    ##Control result
+    result = 1
+    body = {"state":result}
+    response.status_code = 200
+    response.data = body
+    return response #"Hello, World!"
    
 
 
